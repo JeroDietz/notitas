4edefba3b94ed95bb1cf90121746dcb90b6af3f4
JeroDietz <73513239+JeroDietz@users.noreply.github.com>
2021-02-14 18:51:33 -0300

    Terminado

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const methodOverride = require('method-override');
+const indexRouter = require('./routes/router');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.listen(3000, function(){
+    console.log('El servidor esta corriendo en el puerto 3000');
+    console.log('http://localhost:3000');
+})
---
+++ src/controllers/controller.js
@@ -0,0 +1,57 @@
+const db = require('../database/models/index');
+
+module.exports = {
+
+    index: function(req, res){
+        db.Nota.findAll().then(function(notas){
+            return res.render('index', {notas})
+        })
+    },
+
+    create: function (req, res) {
+        db.Nota.create({
+            titulo: req.body.title,
+            texto: req.body.description
+        }).then(function () {
+            res.redirect("/")
+        })
+    },
+
+    detail: function (req, res) {
+        db.Nota.findByPk(req.params.id)
+            .then(function (nota) {
+                res.render("../views/detail", { nota })
+            })
+
+    },
+
+    saveEdit: function (req, res) {
+        db.Nota.update({
+            titulo: req.body.title,
+            texto: req.body.message
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (notaNueva) {
+                res.redirect("/")
+            }).catch(function (e) {
+                res.send(e)
+            })
+    },
+
+    delete: function (req, res) {
+        db.Nota.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (borrada) {
+                res.redirect("/")
+            })
+            .catch(function (errors) {
+                return res.send(errors)
+            })
+    }
+}
